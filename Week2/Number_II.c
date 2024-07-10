c6ceee9a38c68e51e166c26e617884fa50000e0a
Babe <tonnum.kawin@gmail.com>
2024-07-10 15:05:36 +0700

    Fix

--- Week2/Number_II.c
+++ Week2/Number_II.c
@@ -6,10 +6,10 @@ int main()
 
     scanf("%s", number);
 
-    printf("%-80.1s", number);
-    printf("%*s%.2s%*s", 1, "", number, 77, "");
-    printf("%*s%.3s%*s", 2, "", number, 75, "");
-    printf("%*s%.4s%*s", 3, "", number, 73, "");
-    printf("%*s%.5s%*s", 4, "", number, 71, "");
+    printf("%-81.1s", number);
+    printf("%-81.2s", number);
+    printf("%-81.3s", number);
+    printf("%-81.4s", number);
+    printf("%-81.5s", number);
     return 0;
 }
