f44eab5f2b19fb8da45f10fb59784bbac7b1a3d4
Babe <tonnum.kawin@gmail.com>
2024-08-20 12:35:27 +0700

    up

--- Week6/reverse_word.c
+++ Week6/reverse_word.c
@@ -4,6 +4,19 @@ int main()
 {
     char word[101], reverse_word[101];
     scanf("%[^\n]", word);
-    
+    int i = 0, length = 0;
+    while (word[i] != 0) {
+        length++;
+        i++;
+    }
+    for (int j = 0, k = length - 1; j <= length; j++, k--) {
+        if (j == length) {
+            reverse_word[j] = '\0';
+        }
+        else {
+            reverse_word[j] = word[k];
+        }
+    }
+    printf("%s", reverse_word);
     return 0;
 }
